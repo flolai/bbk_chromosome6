ffe34c409fbb87cf1fab566989edcf39a238bb27
OCant86 <57772765+OCant86@users.noreply.github.com>
2020-04-10 16:32:09 +0100

    Update config.py

--- cgi-biocomp2/config.py
+++ cgi-biocomp2/config.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+"""
+This file data contains the database connection info for the dbapi.
+
+Author: Oliver Cant
+Created on 4th April 2020
+
+"""
+
+# Set parameters for connection to MySQL database
+
+dbname   = "co001"
+dbhost   = "localhost"
+dbuser   = "co001"
+dbpass   = "nz8mjl6ft"   
+port     = 3306
 
