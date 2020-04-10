f2302793afc0b685ed399beb171b4023415d0398
maham-96 <61853431+maham-96@users.noreply.github.com>
2020-04-10 17:30:50 +0100

    tablelistall.py

--- cgi-biocomp2/cgi/listall.py
+++ cgi-biocomp2/cgi/listall.py
@@ -1,34 +1,57 @@
-#!/usr/bin/python3
-"""
-...Comment header goes here...
 
-This CGI script obtains all the entries from the BL layer and formats them for 
-HTML display as a table
-"""
+#!/usr/bin/env python3
+
+
+import cgi
+
+#print ("Content-Type: text/html\n")
 
 # Add the bl sub-directory to the module path
 # and the directory above to import the config file
+
 import sys
-sys.path.insert(0, "../bl/")
-sys.path.insert(0, "../")
+sys.path.insert(0, "/d/user6/az001/bl/")
+sys.path.insert(0, "d/user6/az001/WWW/cgi-bin/")
 
 import blapi      # Import the Business Logic API
 import htmlutils  # Import HTML utilities
 import config     # Import configuration information (e.g. URLs)
 
+# Useful debugging output
+import cgitb
+cgitb.enable()
+
+
+
+
+entries = []
 entries = blapi.getAllEntries()
 html    = htmlutils.header()
 
-html += "<h1>Demo list of all entries</h1>\n"
-html += "      <ul>\n"
+html += "<title>Gene Summary</title>"
 
-for entry in entries:
-    html += "        <li>" + entry + "</li>\n"
+#Table Design
 
-html += "      </ul>\n"
+html += "<html><head><style>table {  width:100%;}table, th, td {border: 1px solid black;  border-collapse: collapse;}th, td {  padding: 15px;  text-align: left;}table#t01 tr:nth-child(even) {background-color: #eee;}table#t01 tr:nth-child(odd) { background-color: #fff;}table#t01 th {  background-color: black;  color: white;}</style></head><body>"
+
+
+#Gene summary table page
+
+html += "<table >"
+html += "<tr><th>Accession</th><th>Gene Identifier</th><th>Protein Product</th><th>Chromosome location</th></tr>\n"
+for _ in entries:	
+	html += "<tr>"
+	html += "<td>"+ _['accession']+ "</td>"
+	html += "<td>"+_['gene_id'] + "</td>"	
+	html += "<td>"+_['product'] + "</td>"
+	html += "<td>"+_['location'] + "</td>"
+	html += "</tr>"
+	
+	
+html += "<table>"
+#html += "      </ul>\n"
 html += htmlutils.footer()
 
-print(html)
 
-
-
+print(html)    
+ 
