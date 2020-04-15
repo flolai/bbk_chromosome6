81da9deadfe17ed50ea9864d9763370f93404ef9
maham-96 <61853431+maham-96@users.noreply.github.com>
2020-04-15 19:42:20 +0100

    searchbar.py

--- cgi-biocomp2/cgi/search.py
+++ cgi-biocomp2/cgi/search.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-...Comment header goes here...
-
-This CGI script obtains all the entries from the BL layer and formats them for 
-HTML display as a table
+This CGI script allows user to search using Acession Number and  Restriction Enzyme of your choice
+============
+Author: Maham Ahmad
+Created on 15th April 2020
 """
 
 # Add the bl sub-directory to the module path
@@ -16,15 +16,28 @@ import blapi      # Import the Business Logic API
 import htmlutils  # Import HTML utilities
 import config     # Import configuration information (e.g. URLs)
 
+# Useful debugging output
+import cgitb
+cgitb.enable()  # Send errors to browser
+
 html    = htmlutils.header()
 
-html += "<h1>Dummy search code</h1>\n"
-html += "      <ul>\n"
-html += "        <li>Some search result</li>\n"
-html += "      </ul>\n"
+#Acession = form["accession"]
+
+html += "<html>"
+html += "<h1>Search</h1>"
+html += "<form action= 'http://student.cryst.bbk.ac.uk/cgi-bin/cgiwrap/az001/nextcgi.py' method='get>"
+html += "<label for= 'keyword'> Keyword </label>"
+html += "Keyword: <input type= 'text' id= 'keyword' name= 'keyword' placeholder=\"Search the site...\">  <br/>"
+html += "<label for = 'keyword2'> second name: </label>"
+html += "Keyword2: <input type = 'text' id = 'keyword2' name = 'keyword2'>  <br/>"
+html += "<input type = 'submit' value = 'Submit' />"
+html += "</form>"
+html += "</html>"
+
+
 html += htmlutils.footer()
 
-print(html)
-
+print(html)  
 
 
