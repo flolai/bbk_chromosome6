ad0477937de7bfcb23147a006c904aa3f057d935
maham-96 <61853431+maham-96@users.noreply.github.com>
2020-04-16 21:37:20 +0100

    href for accession

--- cgi-biocomp2/cgi/listall.py
+++ cgi-biocomp2/cgi/listall.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python3
 """
-This CGI script  displays summary of the genes:
-
-Accession number, Gene ID, Protein sequences, chromosomal location.
+This CGI script  displays summary of the genes in a table, with Genbank Accession, Gene Identifier, Protein product and chromosomal location being the column headers. This CGI script also allows users to select any Genbank Accession cell which will take user to summary page of that gene; summary page will display Genbank Accession, Gene Identifier, Protein Product, Amino Acid sequence, Chromosomal Location, Coding region-CDS,  DNA sequences-with coding regions highlighted and star indicating restriction enzyme and codon frequency.
 ============
 Author: Maham Ahmad
 Created on 15th April 2020
 """
-import cgi
-
 #print ("Content-Type: text/html\n")
 
 # Add the bl sub-directory to the module path
 # and the directory above to import the config file
 
 import sys
-sys.path.insert(0, "../bl/")
+sys.path.insert(0, "/d/user6/az001/bl/")
 sys.path.insert(0, "../")
-
 
 import blapi      # Import the Business Logic API
 import htmlutils  # Import HTML utilities
 import config     # Import configuration information (e.g. URLs)
+import cgi
 
 # Useful debugging output
 import cgitb
@@ -32,41 +28,64 @@ entries = []
 entries = blapi.getAllEntries()
 html    = htmlutils.header()
 
-#html += "<title>Gene Summary</title>"
+#html += "<title>Gene Summary Table</title>"
 
 #Table Design
 
-html += "<html>"
-html += "<head>"
-html += "<h1>Gene Summary</h1>"
-html += "<style>"
-html += "table {width:100%;}table, th, td {border: 1px solid black;  border-collapse: collapse;}th, td {  padding: 15px;  text-align: left;} table#t01 tr:nth-child(even) {background-color: #eee;}table#t01 tr:nth-child(odd) { background-color: #fff;}table#t01 th {  background-color: black;  color: white;}"
-html += "</style>"
+html += "<html>\n"
+html += "<head>\n"
+html += "<title>Gene Summary Table</title>\n"
 html += "</head>"
-html += "<body>"
+html += "<body>\n"
+html += "<h1>Gene Summary Table</h1>\n"
+
+html += "<style>"
+html += "table{"
+html += "width:100%;"
+html += "}"
+html += "table, {"
+html += "border: 1px solid black;"
+html += "border-collapse: collapse;"
+html += "}"
+html += "th, td {"
+html += "border: 1px white;"
+html += "padding: 15px;"
+html += "text-align: left;"
+html += "}"
+html += "table#t01 tr:nth-child(even){"
+html += "background-color: #eee;"
+html += "}"
+html += "table#t01 tr:nth-child(odd){"
+html += "background-color: #fff;"
+html += "}"
+html += "table#t01 th {"
+html += "background-color: black;"
+html += "color: white;"
+html += "}"
+html += "</style>"
+
 
 
 #Gene summary table page
 
-html += "<table>"
+html += "<table id =\"t01\">"
 html += "<tr><th>Genbank Accession</th>"
 html += "<th>Gene Identifier</th>"
 html += "<th>Protein Product</th>"
 html += "<th>Chromosomal Location</th>"
 html += "</tr>\n"
 for _ in entries:	
-	html += "<tr>"
-	html += "<td>"+ _['accession']+ "</td>"
+	html += "<tr\n>"
+	html += "<td><a href=\"http://student.cryst.bbk.ac.uk/cgi-bin/cgiwrap/az001/nextcgi.py\" method=\"get\"id=\"accession\">"+ _['accession']+ "</a></td>"
 	html += "<td>"+_['gene_id'] + "</td>"	
 	html += "<td>"+_['product'] + "</td>"
 	html += "<td>"+_['location'] + "</td>"
-	html += "</tr>"
+	html += "</tr>\n"
 	
 	
-html += "<table>"
-#html += "      </ul>\n"
+html += "</table>\n"
+html += "</body>\n"
+html += "</html>\n"
 html += htmlutils.footer()
 
-
-print(html) 
- 
+print(html)   
