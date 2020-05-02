5954aae5c40057ba319db3bae314781140d981fa
Florence Lai <flolai@gmail.com>
2020-05-02 23:18:18 +0100

    updating listall testing

--- testing/listall_testing.py
+++ testing/listall_testing.py
@@ -42,65 +42,74 @@ V1.0   	10.04.20   Original   By: Maham Ahmad
 """
 #*****************************************************************************************************************************************************************************************
 # Import libraries
-import cgi
-print ("Content-Type: text/html\n")
+
+import unittest
+import sys
+class htmlTestCase(unittest.TestCase):
+    def test_html(self, MSG, file = sys.stdout):
+        
+        import cgi
+        print ("Content-Type: text/html\n")
 
 # Add the bl sub-directory to the module path
 # and the directory above to import the config file
 
-import sys
-sys.path.insert(0, "/d/user6/az001/bl/")
-sys.path.insert(0, "/d/user6/az001/db/") 
+    
+    sys.path.insert(0, "/d/user6/az001/bl/")
+    sys.path.insert(0, "/d/user6/az001/db/") 
 
-import blapi      # Import the Business Logic API
-import htmlutils  # Import HTML utilities
-import config     # Import configuration information (e.g. URLs)
+    import blapi      # Import the Business Logic API
+    import htmlutils  # Import HTML utilities
+    import config     # Import configuration information (e.g. URLs)
 
 
 # Useful debugging output
-import cgitb
-cgitb.enable()
+    import cgitb
+    cgitb.enable()
 
 #*****************************************************************************************************************************************************************************************
 
 # Grab the content of the form
-form = cgi.FieldStorage()
+    form = cgi.FieldStorage()
 
-accession = form.getvalue("accession")
-gene_id = form.getvalue("gene_id")
-product = form.getvalue("product")
-location = form.getvalue("location")
-entries = blapi.getAllEntries(accession = accession, gene_id = gene_id, product = product,location = location)
+    accession = form.getvalue("accession")
+    gene_id = form.getvalue("gene_id")
+    product = form.getvalue("product")
+    location = form.getvalue("location")
+    entries = blapi.getAllEntries(accession = accession, gene_id = gene_id, product = product,location = location)
 
 
 
 
 # Header and navigation
 
-html    = htmlutils.header()
-html += htmlutils.navigation()
+    html    = htmlutils.header()
+    html += htmlutils.navigation()
 
 
 # Gene summary page table
 
-html += "<h3>Gene Summary Table:</h3>\n"
-html += "<div class='summary-page'>\n"
-html += "<table class ='summary'>\n"
-html += "<tr>\n"
-html += "<th>Genbank Accession</th>\n"
-html += "<th>Gene Identifier</th>\n"
-html += "<th>Protein Product</th>\n"
-html += "<th>Chromosomal Location</th>\n"
-html += "</tr>\n"
-for _ in entries:	
-	html += "<tr>\n"
-	html += "<td><a href='http://student.cryst.bbk.ac.uk/cgi-bin/cgiwrap/az001/search.py?accession={}' method='get'>".format(_['accession'])+ _['accession']+ "</a></td>\n" 
-	html += "<td>"+_['gene_id'] + "</td>\n"	
-	html += "<td>"+_['product'] + "</td>\n"
-	html += "<td>"+_['location'] + "</td>\n"
-	html += "</tr>\n"
-html += "</table>\n"
+    html += "<h3>Gene Summary Table:</h3>\n"
+    html += "<div class='summary-page'>\n"
+    html += "<table class ='summary'>\n"
+    html += "<tr>\n"
+    html += "<th>Genbank Accession</th>\n"
+    html += "<th>Gene Identifier</th>\n"
+    html += "<th>Protein Product</th>\n"
+    html += "<th>Chromosomal Location</th>\n"
+    html += "</tr>\n"
+    for _ in entries:	
+        html += "<tr>\n"
+        html += "<td><a href='http://student.cryst.bbk.ac.uk/cgi-bin/cgiwrap/az001/search.py?accession={}' method='get'>".format(_['accession'])+ _['accession']+ "</a></td>\n" 
+        html += "<td>"+_['gene_id'] + "</td>\n"	
+        html += "<td>"+_['product'] + "</td>\n"
+        html += "<td>"+_['location'] + "</td>\n"
+        html += "</tr>\n"
+    html += "</table>\n"
 
-html += htmlutils.footer()
+    html += htmlutils.footer()
 
-print(html)   
+    #print(html)   
+print(MSG, file=file)
+if __name__ == '__main__':
+    unittest.main()
