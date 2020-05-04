eb50b765f19a45066579a7695a808fdaada05a77
maham-96 <61853431+maham-96@users.noreply.github.com>
2020-05-04 16:04:25 +0100

    Update htmlutils.py

--- cgi-biocomp2/cgi/htmlutils.py
+++ cgi-biocomp2/cgi/htmlutils.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-Program:    HTMLUTILS script
+Program:    HTML UTILS script
 File:       htmlutils.py
 
-Version:    V2.0
-Date:       03.04.2020
+Version:    V3.0
+Date:       04.05.2020
 Function:   Support code for generating HTML files
 
 Copyright:  (c) Maham Ahmad, Birckbeck, 2020
@@ -13,33 +13,30 @@ Author:     Maham Ahmad
 ------------------------------------------------------------------------------------------
 This program is released under the GNU Public Licence (GPL V3)
 ------------------------------------------------------------------------------------------
-============
-Description:
-============
-
 
 Revision History:
 =================
-V1.0   	01.04.20   Original   By: Maham Ahmad
+V1.0   	01.05.2020   Original   By: Maham Ahmad
+V2.0   	02.05.2020
+V3.0    04.05.2020     
 
 """
+#******************************************************************************************************************************************************************
 
 def header():
     """
     This function generates HTML code for the header in both search.py and listall.py
     """
     html = "Content-Type: text/html\n\n"   # MIME-Type header
-    html += "<!DOCTYPE html>"
+    html += "<!DOCTYPE html>\n"
     html += "<html lang='en'>\n"
     html += "  <head>\n"
     html += "    <title>Chromosome 6 Genome Browser</title>\n"
     html += "	 <meta name='viewport' content='width=device-width, initial-scale=1'>\n"	
     html += "    <link rel='stylesheet' type='text/css' href='http://student.cryst.bbk.ac.uk/~az001/biocomp2.css' />\n"
-    html += "	 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>\n"
     html += "    <link rel = 'stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css'>\n"
     html += "    <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap' rel='stylesheet'>\n"
     html += "  </head>\n"
-    html += "  \n"
     html += "  <body>\n"
 
 
